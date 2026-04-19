dc0e7f0272873136ce7bbe1f6b5e1e72b89babf7
max089411-alt <max089411@gmail.com>
2026-04-19 21:06:40 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+{
+  "name": "ozon-shop",
+  "version": "1.0.0",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.0",
+    "node-telegram-bot-api": "^0.61.0"
+  }
+}
